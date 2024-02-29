0bcb14e498a10d5217a0a4fcba5284c7fb6fbaea
Tetiana <tzasimenko@gmail.com>
2024-02-29 03:57:13 +0000

    Initialize quiz score and select DOM elements for correct answer, total questions, and try-again button

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,19 @@ const option1 = document.querySelector('.option1'),
       option2 = document.querySelector('.option2'),
       option3 = document.querySelector('.option3'),
       option4 = document.querySelector('.option4');
+
+ 
+const question = document.getElementById('question'),
+numberOfQuestion = document.getElementById('number-of-question'),
+numberOfAllQuestion = document.getElementById('number-of-all-questions');
+
+let indexOfQuestion, // index of the current question
+indexOfPage = 0; // page index
+
+const btnNext = document.getElementById('btn-next');
+
+let score = 0; //result of qwiz
+
+const correctAnswer = document.getElementById('correct-answer'),
+      numberOfAllQuestion2 = document.getElementById('number-of-all-questions-2'),
+      btnTryAgain = document.getElementById('btn-try-again');
